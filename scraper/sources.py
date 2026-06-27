f20f6f2ac61663da9f30664ea73639929f6be09a
Gulamo_Jamal <gulamojamal@gmail.com>
2026-06-27 09:35:31 +0200

    Update sources.py

--- scraper/sources.py
+++ scraper/sources.py
@@ -1,7 +1,78 @@
 # Lista de fontes de vagas em Moçambique
 SOURCES = [
+    # =========================
+    # AGREGADORES DE EMPREGO
+    # =========================
     {
-        "name": "mozempregos",
-        "url": "https://www.mozempregos.com/"
+        "name": "mmo_emprego",
+        "url": "https://emprego.mmo.co.mz"
+    },
+    {
+        "name": "emprego_co_mz",
+        "url": "https://www.emprego.co.mz"
+    },
+    {
+        "name": "todas_vagas",
+        "url": "https://todasvagas.com"
+    },
+    {
+        "name": "vagas_emprego_moz",
+        "url": "https://vagademprego.com"
+    },
+    {
+        "name": "mais_vagas",
+        "url": "https://maisvagas.co.mz"
+    },
+    {
+        "name": "so_vagas",
+        "url": "https://sovagas.co.mz"
+    },
+    {
+        "name": "vagas_moz",
+        "url": "https://vagasmoz.com"
+    },
+    {
+        "name": "njobs",
+        "url": "https://njobs.co.mz"
+    },
+
+    # =========================
+    # EMPRESAS
+    # =========================
+    {
+        "name": "totalenergies",
+        "url": "https://jobs.totalenergies.com/pt_BR/careers/Home"
+    },
+    {
+        "name": "vodacom_mz",
+        "url": "https://careers.vodafone.com/mozambique/"
+    },
+    {
+        "name": "millennium_bim",
+        "url": "https://www.millenniumbim.co.mz/pt/institucional/o-banco/carreira"
+    },
+    {
+        "name": "bci",
+        "url": "https://www.bci.co.mz/recrutamento/"
+    },
+    {
+        "name": "moz_lng",
+        "url": "https://www.mozambiquelng.co.mz/opportunities/working-with-us/"
+    },
+
+    # =========================
+    # ONGs / INTERNACIONAL
+    # =========================
+    {
+        "name": "reliefweb",
+        "url": "https://reliefweb.int/jobs?country=MOZ"
+    },
+    {
+        "name": "un_jobs",
+        "url": "https://unjobs.org/duty_stations/mozambique"
+    },
+    {
+        "name": "contact_mz",
+        "url": "https://www.contact.co.mz"
     }
 ]
