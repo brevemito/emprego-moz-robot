4fe16af36837b9a0bb9853c1a944254af6dc9cbe
Gulamo_Jamal <gulamojamal@gmail.com>
2026-07-01 14:44:33 +0200

    Update sources.py

--- scraper/sources.py
+++ scraper/sources.py
@@ -1,78 +1,67 @@
 # Lista de fontes de vagas em Moçambique
+
 SOURCES = [
+
     # =========================
-    # AGREGADORES DE EMPREGO
+    # PORTAIS DE EMPREGO
     # =========================
-    {
-        "name": "mmo_emprego",
-        "url": "https://emprego.mmo.co.mz"
-    },
+
     {
         "name": "emprego_co_mz",
         "url": "https://www.emprego.co.mz"
     },
-    {
-        "name": "todas_vagas",
-        "url": "https://todasvagas.com"
-    },
-    {
-        "name": "vagas_emprego_moz",
-        "url": "https://vagademprego.com"
-    },
-    {
-        "name": "mais_vagas",
-        "url": "https://maisvagas.co.mz"
-    },
-    {
-        "name": "so_vagas",
-        "url": "https://sovagas.co.mz"
-    },
-    {
-        "name": "vagas_moz",
-        "url": "https://vagasmoz.com"
-    },
+
     {
         "name": "njobs",
         "url": "https://njobs.co.mz"
     },
 
+    {
+        "name": "contact_mz",
+        "url": "https://www.contact.co.mz"
+    },
+
     # =========================
     # EMPRESAS
     # =========================
+
     {
         "name": "totalenergies",
         "url": "https://jobs.totalenergies.com/pt_BR/careers/Home"
     },
+
     {
         "name": "vodacom_mz",
         "url": "https://careers.vodafone.com/mozambique/"
     },
+
     {
         "name": "millennium_bim",
         "url": "https://www.millenniumbim.co.mz/pt/institucional/o-banco/carreira"
     },
+
     {
         "name": "bci",
         "url": "https://www.bci.co.mz/recrutamento/"
     },
+
     {
-        "name": "moz_lng",
+        "name": "mozambique_lng",
         "url": "https://www.mozambiquelng.co.mz/opportunities/working-with-us/"
     },
 
     # =========================
-    # ONGs / INTERNACIONAL
+    # ORGANIZAÇÕES INTERNACIONAIS
     # =========================
+
     {
         "name": "reliefweb",
         "url": "https://reliefweb.int/jobs?country=MOZ"
     },
+
     {
-        "name": "un_jobs",
+        "name": "unjobs",
         "url": "https://unjobs.org/duty_stations/mozambique"
-    },
-    {
-        "name": "contact_mz",
-        "url": "https://www.contact.co.mz"
     }
+
 ]
