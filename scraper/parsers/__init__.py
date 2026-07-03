ca8e8ac4760db6782fb989af6f80a4a71f031fe2
Gulamo_Jamal <gulamojamal@gmail.com>
2026-07-03 10:43:35 +0200

    Update __init__.py

--- scraper/parsers/__init__.py
+++ scraper/parsers/__init__.py
@@ -1,10 +1,12 @@
 from parsers.emprego_co_mz import parse_emprego_co_mz
 from parsers.totalenergies import parse_totalenergies
 from parsers.unjobs import parse_unjobs
+from parsers.reliefweb import parse_reliefweb
 
 
 PARSERS = {
     "emprego_co_mz": parse_emprego_co_mz,
     "totalenergies": parse_totalenergies,
-    "unjobs": parse_unjobs
+    "unjobs": parse_unjobs,
+    "reliefweb": parse_reliefweb
 }
