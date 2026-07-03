059001b73b4d22fe83c85c0e4272bb1dcc9f1ead
Gulamo_Jamal <gulamojamal@gmail.com>
2026-07-03 10:38:43 +0200

    Add unjobs parser to the PARSERS dictionary

--- scraper/parsers/__init__.py
+++ scraper/parsers/__init__.py
@@ -1,8 +1,10 @@
 from parsers.emprego_co_mz import parse_emprego_co_mz
 from parsers.totalenergies import parse_totalenergies
+from parsers.unjobs import parse_unjobs
 
 
 PARSERS = {
     "emprego_co_mz": parse_emprego_co_mz,
-    "totalenergies": parse_totalenergies
+    "totalenergies": parse_totalenergies,
+    "unjobs": parse_unjobs
 }
