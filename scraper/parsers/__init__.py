68beef20b4dd70b570d4fb229f55a3b2a1bbe35f
Gulamo_Jamal <gulamojamal@gmail.com>
2026-07-03 12:33:03 +0200

    Update __init__.py

--- scraper/parsers/__init__.py
+++ scraper/parsers/__init__.py
@@ -3,6 +3,7 @@ from parsers.totalenergies import parse_totalenergies
 from parsers.unjobs import parse_unjobs
 from parsers.reliefweb import parse_reliefweb
 from parsers.vodacom_mz import parse_vodacom_mz
+from parsers.bci import parse_bci
 
 
 PARSERS = {
@@ -10,5 +11,6 @@ PARSERS = {
     "totalenergies": parse_totalenergies,
     "unjobs": parse_unjobs,
     "reliefweb": parse_reliefweb,
-    "vodacom_mz": parse_vodacom_mz
+    "vodacom_mz": parse_vodacom_mz,
+    "bci": parse_bci
 }
