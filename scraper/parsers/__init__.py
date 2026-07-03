069789222db6a08074c28ec33fc1f940df7e69a1
Gulamo_Jamal <gulamojamal@gmail.com>
2026-07-03 10:17:16 +0200

    Update __init__.py

--- scraper/parsers/__init__.py
+++ scraper/parsers/__init__.py
@@ -1,6 +1,8 @@
 from parsers.emprego_co_mz import parse_emprego_co_mz
+from parsers.totalenergies import parse_totalenergies
 
-# REGISTRO CENTRAL DE PARSERS
+
 PARSERS = {
-    "emprego_co_mz": parse_emprego_co_mz
+    "emprego_co_mz": parse_emprego_co_mz,
+    "totalenergies": parse_totalenergies
 }
